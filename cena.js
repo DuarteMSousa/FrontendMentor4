a583a773d939216172a9c9c9e719dc9988a18d99
Duarte Sousa <126507544+DuarteMSousa@users.noreply.github.com>
2023-09-19 19:32:30 +0100

    a

--- cena.js
+++ cena.js
@@ -1,6 +1,26 @@
-function mudarMensagem() {
-    let numero=document.getElementById("num")
-    let mensagem=document.getElementById("msg")
+/*
+function getAdvice() {
+    let conteudo
+    fetch('https://api.adviceslip.com/advice').then(res => res.json()).then(data => {
+        console.log(data)
+        let conteudo = ["" + data.slip.id, data.slip.advice]
+        console.log(conteudo)
 
-    numero.innerHTML("sou lindo")
+    }
+    )
+    return conteudo
 }
+*/
+
+
+function mudarMensagem() {
+    let numero = document.getElementById("num");
+    let mensagem = document.getElementById("msg");
+    let conteudo
+    fetch('https://api.adviceslip.com/advice').then(res => res.json()).then(data => {    
+        numero.innerHTML = "ADVICE #" + data.slip.id;
+        mensagem.innerHTML = data.slip.advice;
+    }
+    )
+}
+
